27a11335ac16d46580555da3e3514446d838de44
goatwu1993 <peteeelol@gmail.com>
2023-04-16 17:54:52 +0800

    feat: add error level

--- flake8_utils_reporters/github.py
+++ flake8_utils_reporters/github.py
@@ -2,6 +2,7 @@
 GitHub Formatters.
 """
 
+from enum import Enum
 from typing import Optional
 
 from flake8.violation import Violation
@@ -9,15 +10,35 @@ from flake8.violation import Violation
 from .base import BaseUtilsReporter
 
 
+class GitHubErrorLevelEnum(str, Enum):
+    ERROR = "error"
+    NOTICE = "notice"
+
+
 class GitHub(BaseUtilsReporter):
     """GitHub formatter for Flake8."""
 
     error_format = (
-        "::error title=Flake8 %(code)s,file=%(path)s,"
+        "::%(error_level)s title=Flake8 %(code)s,file=%(path)s,"
         "line=%(row)d,col=%(col)d,endLine=%(row)d,endColumn=%(col)d"
         "::%(code)s %(text)s"
     )
     git_relative_path = False
+
+    @classmethod
+    def add_options(cls, option_manager) -> None:
+        super().add_options(option_manager)
+        option_manager.add_option(
+            "--error-level",
+            default=GitHubErrorLevelEnum.ERROR.value,
+            choices=[e.value for e in GitHubErrorLevelEnum],
+            help=("error-level"),
+        )
+
+    @classmethod
+    def parse_options(cls, option_manager) -> None:
+        super().parse_options(option_manager)
+        cls.error_level = option_manager.error_level
 
     def format(self, error: Violation) -> Optional[str]:
         """Format and write error out.
@@ -26,6 +47,7 @@ class GitHub(BaseUtilsReporter):
         file. Otherwise, print the formatted error to standard out.
         """
         return self.error_format % {
+            "error_level": self.error_level,
             "code": error.code,
             "text": error.text,
             "path": self.to_repo_relative_path(error.filename)
